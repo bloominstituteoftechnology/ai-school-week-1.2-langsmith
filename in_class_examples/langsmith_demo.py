b5a4f0a2353f477dcff0408f318a3997f66d1137
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-06-25 14:25:39 -0300

    HOTFIX: INDENTION
    - in langsmith_demo.py:
    - fix indentions in `pipeline` fn

--- in_class_examples/langsmith_demo.py
+++ in_class_examples/langsmith_demo.py
@@ -16,11 +16,11 @@ client = wrap_openai(openai.Client())
 
 @traceable # Auto-trace this function
 def pipeline(user_input: str):
-   result = client.chat.completions.create(
-       messages=[{"role": "user", "content": 
-             user_input}],
-       model="gpt-3.5-turbo")
-   return result.choices[0].message.content
+	result = client.chat.completions.create(
+		messages=[{"role": "user", "content":
+			user_input}],
+		model="gpt-3.5-turbo")
+	return result.choices[0].message.content
 
 print(pipeline("Hello, world!"))
 # Out:  Hello there! How can I assist you today?
